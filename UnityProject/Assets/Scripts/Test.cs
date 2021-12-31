3a21eadab14daa962c0d35a07bb9dd0b89453f4f
mzbswh <mzbswh@gmail.com>
2021-12-31 13:19:21 +0800

    测试通过流读取数据

--- UnityProject/Assets/Scripts/Test.cs
+++ UnityProject/Assets/Scripts/Test.cs
@@ -6,6 +6,8 @@ using Debug = UnityEngine.Debug;
 using System.Numerics;
 using UnityEngine.Rendering;
 using ExcelTool;
+using System.IO;
+using System.Text;
 
 public class Test : MonoBehaviour
 {
@@ -22,6 +24,25 @@ public class Test : MonoBehaviour
         {
             foreach (var j in i)
                 Debug.LogError($"{j.Key}  {j.Value}");
+        }
+
+
+        // 测试直接通过流读取数据
+        byte[] cache = new byte[100];
+        using (FileStream fs = File.Open(Application.dataPath + "/Resources/ByteData/Test_s_ttt.bytes", FileMode.Open))
+        {
+            fs.Seek(4, SeekOrigin.Begin);
+            fs.Read(cache, 0, 4);
+            var addr = ByteReader.Read<int>(cache, 0);
+            Debug.LogError("addr = " + addr);
+            fs.Seek(addr, SeekOrigin.Begin);
+            cache = new byte[20];
+            fs.Read(cache, 0, 20);
+        }
+        var ls = ByteReader.ReadListInt(cache, 0, false);
+        foreach (var i in ls)
+        {
+            Debug.LogError(i);
         }
     }
 }
